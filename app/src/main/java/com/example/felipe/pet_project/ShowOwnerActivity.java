d2ef3778060906b69d2f816dc5ee554535b9f4b8
Felipe <fbarrios.ing.informatico@gmail.com>
2017-05-04 23:28:45 -0300

    ADD: hotfix

--- app/src/main/java/com/example/felipe/pet_project/ShowOwnerActivity.java
+++ app/src/main/java/com/example/felipe/pet_project/ShowOwnerActivity.java
@@ -1,11 +1,12 @@
 package com.example.felipe.pet_project;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class ShowOwnerActivity extends AppCompatActivity {
 
@@ -13,17 +14,31 @@ public class ShowOwnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_owner);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.menuHome:
+                Intent intentHome = new Intent(ShowOwnerActivity.this, HomeActivity.class);
+                startActivity(intentHome);
+                break;
+            case R.id.menuFormOwner:
+                Intent intentFormOwner = new Intent(ShowOwnerActivity.this, OwnerActivity.class);
+                startActivity(intentFormOwner);
+                break;
+            case R.id.menuFormPet:
+                Intent intentFormPet = new Intent(ShowOwnerActivity.this, PetActivity.class);
+                startActivity(intentFormPet);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/felipe/pet_project/ShowPetActivity.java
+++ app/src/main/java/com/example/felipe/pet_project/ShowPetActivity.java
@@ -1,11 +1,11 @@
 package com.example.felipe.pet_project;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ShowPetActivity extends AppCompatActivity {
 
@@ -13,17 +13,32 @@ public class ShowPetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_pet);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.menuHome:
+                Intent intentHome = new Intent(ShowPetActivity.this, HomeActivity.class);
+                startActivity(intentHome);
+                break;
+            case R.id.menuFormOwner:
+                Intent intentFormOwner = new Intent(ShowPetActivity.this, OwnerActivity.class);
+                startActivity(intentFormOwner);
+                break;
+            case R.id.menuFormPet:
+                Intent intentFormPet = new Intent(ShowPetActivity.this, PetActivity.class);
+                startActivity(intentFormPet);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
