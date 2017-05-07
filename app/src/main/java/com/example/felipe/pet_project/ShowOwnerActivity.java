cd41f66e7ecf54e4efadd7acbf627c3a1e15b748
Felipe <fbarrios.ing.informatico@gmail.com>
2017-05-06 22:30:56 -0300

    ADD: recicler view for owner

--- app/src/main/java/com/example/felipe/pet_project/ShowOwnerActivity.java
+++ app/src/main/java/com/example/felipe/pet_project/ShowOwnerActivity.java
@@ -3,17 +3,32 @@ package com.example.felipe.pet_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.example.felipe.pet_project.Adapter.OwnerAdapter;
+import com.example.felipe.pet_project.db.dbHelper;
 
 public class ShowOwnerActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    dbHelper dbHelper;
+    OwnerAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_owner);
+
+        dbHelper = dbHelper.getInstance(getApplicationContext());
+
+        recyclerView = (RecyclerView) findViewById(R.id.owner_recicler_view);
+        adapter = new OwnerAdapter(this, dbHelper.getAllOwners());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
